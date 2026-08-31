fd465dc3ffa61cfa566be427f3d5cee2bf1ac360
jiaqing8090 <jiaqing8090@users.noreply.github.com>
2026-08-31 12:26:45 +0800

    chore: clear repository contents

--- tg-hub/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""tg-hub scripts package"""
-from .client import TGClient
-from .exceptions import ChatNotFoundError, NotAuthenticatedError, SyncError, TGHubError
-
-__all__ = ["TGClient", "TGHubError", "NotAuthenticatedError", "ChatNotFoundError", "SyncError"]
--- weibo-hub/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-"""weibo-hub scripts package."""
